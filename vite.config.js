c888b1a2696e744d7a2b3d526f32215c3fd95611
niten01 <ovcharov_05@mail.ru>
2026-03-13 17:32:12 +0300

    fix Pages base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({command}) => ({
   plugins: [vue()],
-})
+  base: command === 'build' ? '/clid/' : '/',
+  server: {
+    port: 5173,
+  }
+}));
+
